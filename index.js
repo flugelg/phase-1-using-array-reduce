ebc71d45e0fe881908d56a43cc4d7badc4701210
Gary Flugel <flugelg@gmail.com>
2021-12-10 05:30:39 -0500

    commit tree f3fe41197c5d2dbfc879d99750d0de449e99ccd2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+/*
+function totalBatteries(batteryBatches){
+    return sum;
+}
+*/
+
+
+const totalBatteries = batteryBatches.reduce(function(accumulator, currentValue){
+    return accumulator += currentValue;
+}, 0);
